2f6f8123eed8243aa1a5f46b712643f24768b0c1
donaldonana <onanadonald@gmail.com>
2023-02-02 15:48:11 +0100

    lstm

--- LSTM/sequentiel/src/app.c
+++ LSTM/sequentiel/src/app.c
@@ -8,42 +8,79 @@
 #include <string.h>
 #include <pthread.h>
 
-#include "std_conf.h"
 
 
-lstm_model_parameters params;
+
+float lr;
+int MINI_BATCH_SIZE, NUM_THREADS, epoch  ;
+
+
+void parse_input_args(int argc, char** argv)
+{
+  int a = 0;
+
+  while ( a < argc ) {
+
+    if ( argc <= (a+1) ){ 
+      break; // All flags have values attributed to them
+    }
+
+    if ( !strcmp(argv[a], "-lr") ) {
+      lr = atof(argv[a + 1]);
+      if ( lr == 0.0 ) {
+        // usage(argv);
+      }
+    } else if ( !strcmp(argv[a], "-thread") ) {
+      NUM_THREADS = atoi(argv[a + 1]);
+      if ( NUM_THREADS <= 0 ) {
+        // usage(argv);
+      }
+    } else if ( !strcmp(argv[a], "-epoch") ) {
+      epoch = (unsigned long) atoi(argv[a + 1]);
+      if ( epoch == 0 ) {
+        // usage(argv);
+      }
+    } else if ( !strcmp(argv[a], "-batch") ) {
+      MINI_BATCH_SIZE = (unsigned long) atoi(argv[a + 1]);
+      if ( MINI_BATCH_SIZE == 0 ) {
+        // usage(argv);
+      }
+    }  
+    a += 1;
+
+  }
+}
 
 
 
 int main()
 {
 
-  memset(&params, 0, sizeof(params));
-  params.learning_rate = 0.01;
-  params.softmax_temp = SOFTMAX_TEMP;
-  params.mini_batch_size = MINI_BATCH_SIZE;
   Data *data  = malloc(sizeof(Data));
   get_data(data);
-
+  lr = 0.01;
+  MINI_BATCH_SIZE = 1;
   int X = data->ecol;
   int N = 64, Y = 2;
-  int epoch = 15 ;
+  epoch = 15 ;
   
   lstm_rnn* lstm = e_calloc(1, sizeof(lstm_rnn));
   lstm_rnn* gradient = e_calloc(1, sizeof(lstm_rnn));
   lstm_rnn* AVGgradient = e_calloc(1, sizeof(lstm_rnn));
   lstm_values_cache **cache = alloc_cache_array(X, N, Y, data->xcol);
-  lstm_init_model(X, N, Y , lstm, 0, &params); 
-  lstm_init_model(X, N, Y , gradient , 1, &params);
-  lstm_init_model(X, N, Y , AVGgradient , 1, &params);
+  lstm_init_model(X, N, Y , lstm, 0); 
+  lstm_init_model(X, N, Y , gradient , 1);
+  lstm_init_model(X, N, Y , AVGgradient , 1);
 
   double *h_prev = get_zero_vector(lstm->N);
   double *c_prev = get_zero_vector(lstm->N);
 
+  print_summary(lstm, epoch, MINI_BATCH_SIZE, lr, NUM_THREADS);
+  printf("\n====== Training =======\n");
   for (int e = 0; e < epoch ; e++)
   {
     printf("\nStart of epoch %d/%d \n", (e+1) , epoch);
-    lstm_training(lstm, gradient, AVGgradient, data, cache, h_prev, c_prev);
+    lstm_training(lstm, gradient, AVGgradient, MINI_BATCH_SIZE, lr, data, cache, h_prev, c_prev);
   }
  
   lstm_free_model(lstm);
